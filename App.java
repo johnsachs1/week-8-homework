1ebc895982e4945393b3fb0a36c801911c462238
johnsachs1 <97467168+johnsachs1@users.noreply.github.com>
2022-03-08 17:15:59 -0500

    Add files via upload

---
+++ App.java
@@ -0,0 +1,33 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+        int myNumber;
+
+        short myShort = 847;
+        long myLong = 9797;
+        
+        double myDouble = 7.3243;
+        float myFloat = 324.3f;
+        char myChar = 'y';
+        boolean myBoolean = false;
+        byte myByte = 127;
+
+
+
+        myNumber = 88;
+
+        System.out.println(myNumber);
+
+        int myInt = 7;
+
+        String text = "Hello";
+
+        String blank = " ";
+
+        String name = "jake";
+
+        String greeting = text + blank + name;
+
+        System.out.println("My integer is : " + myInt);
+    }
+}
